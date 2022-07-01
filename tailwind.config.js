faef16a10099f4789df1986f5c1d5d24c8c05eb2
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-01 21:49:46 +0530

    feat(search): :sparkles: add color palatte in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		colors: {
+			dark: {
+				100: '#6D81A1',
+				900: '#041F1E'
+			},
+			skin: '#F9B9B0',
+			grey: {
+				50: '#E9F3FB',
+				100: '#E4F0FA'
+			}
+		},
+		extend: {
+			boxShadow: {
+				lg: '-2px -2px 4px rgba(255, 255, 255, 0.5), 2px 2px 4px rgba(109, 129, 161, 0.25)'
+			}
+		}
 	},
 	plugins: []
 }
