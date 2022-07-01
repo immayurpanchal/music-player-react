bc132a17659e8ce137ae520be4e42c53842e9ebc
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-02 00:05:40 +0530

    feat(search): :sparkles: add search api

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,28 @@
+import { useRef, useState } from 'react'
+import useSWR from 'swr'
+import fetch from 'unfetch'
 import BackChevron from '../components/Icons/BackChevron'
 import KeyboardIcon from '../components/Icons/Keyboard'
 import SearchIcon from '../components/Icons/SearchIcon'
 
+const fetcher = url => fetch(url).then(r => r.json())
+
 const SearchPage = () => {
+	const inputRef = useRef<HTMLInputElement>(null)
+	const [searchValue, setSearchValue] = useState('')
+	const { data, error } = useSWR(
+		searchValue
+			? `https://saavn.me/search/songs?query=${searchValue}&page=1&limit=5`
+			: '',
+		fetcher
+	)
+
+	if (error) {
+		return <div>failed to load</div>
+	}
+
 	return (
-		<div className='grid grid-rows-4 gap-y-7'>
+		<div className='flex flex-col gap-y-7 grid-flow-row'>
 			<div className='flex justify-between items-center'>
 				<div className='w-12 h-12 rounded-full flex justify-center items-center neumorphism'>
 					<BackChevron />
@@ -13,22 +31,40 @@ const SearchPage = () => {
 				<KeyboardIcon />
 			</div>
 			<div className='relative'>
-				{/* <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'> */}
 				<span className='absolute inset-y-4 pl-6 text-gray-500 sm:text-sm pointer-events-none'>
 					<SearchIcon />
 				</span>
-				{/* </div> */}
-				<input
-					className='py-3 pl-14 rounded-2xl pr-4 bg-transparent w-full neumorphism focus-visible:outline-0 text-lg '
-					placeholder='Search Your Music'
-					type='search'
-				/>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						setSearchValue(inputRef.current?.value || '')
+					}}
+				>
+					<input
+						className='py-3 pl-14 rounded-2xl pr-4 bg-transparent w-full neumorphism focus-visible:outline-0 text-lg placeholder:text-dark-100 '
+						placeholder='Search Your Music'
+						type='search'
+						ref={inputRef}
+					/>
+				</form>
 			</div>
-			<span>Recently Searched</span>
-			<div>
-				<img src='' alt='' />
-				<span>Sara Sari</span>
-				<span>Anurag Kulkarni</span>
+			<span className='text-lg text-dark-100'>Recently Searched</span>
+			<div className='flex flex-col gap-10'>
+				{data?.results?.map(song => {
+					const { image, name, id, year } = song
+					return (
+						<div className='flex items-center gap-3' key={id}>
+							<img
+								src={image?.[2]?.link}
+								className={`rotate-45 w-[60px] h-[60px] rounded-xl bg-cover row-span-2 mx-4`}
+							/>
+							<div className='flex flex-col'>
+								<span className='text-base'>{name}</span>
+								<span className='text-xs text-dark-100'>{year}</span>
+							</div>
+						</div>
+					)
+				})}
 			</div>
 		</div>
 	)
