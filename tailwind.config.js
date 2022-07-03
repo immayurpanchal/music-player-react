efbb8e5ebf81c2b282fb0f324faf9e0ec5ca806e
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-03 22:23:01 +0530

    chore(component): :wrench: update skin color & slider color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
 				100: '#6D81A1',
 				900: '#041F1E'
 			},
-			skin: '#F9B9B0',
+			skin: { 100: '#F9B9B0', 500: '#DE958B' },
 			grey: {
 				50: '#E9F3FB',
 				100: '#E4F0FA'
@@ -17,7 +17,8 @@ module.exports = {
 			boxShadow: {
 				lg: '-2px -2px 4px rgba(255, 255, 255, 0.5), 2px 2px 4px rgba(109, 129, 161, 0.25)',
 				inset:
-					'inset -8px -8px 6px rgba(255, 255, 255, 0.5), inset 8px 8px 10px rgba(109, 129, 161, 0.16)'
+					'inset -4px -4px 6px rgba(255, 255, 255, 0.5), inset 4px 4px 6px rgba(109, 129, 161, 0.16)',
+				'inset-slider': 'inset 0px 4px 4px rgba(4, 31, 30, 0.25)'
 			},
 			borderRadius: {
 				'4xl': '2rem'
