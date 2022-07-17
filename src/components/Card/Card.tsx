f25db6855cba40d67026a585e58e4b84ddebd419
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-17 22:36:48 +0530

    feat(home): :sparkles: implement card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,17 @@
-const Card = () => {
-	return <>Card</>
+import Typography from '../Typography/Typography'
+type Props = {
+	title: string
+	image: string
+}
+
+const Card = (props: Props) => {
+	const { title, image } = props
+	return (
+		<div className='flex flex-col gap-y-3 w-44 neumorphism p-2 rounded-2xl shrink-0'>
+			<img alt={title} className='rounded-2xl' src={image} />
+			<Typography type='caption'>{title}</Typography>
+		</div>
+	)
 }
 
 export default Card
