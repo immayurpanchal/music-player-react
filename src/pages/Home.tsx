1dac5a91d489e107c5103066948c693911e665c0
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-17 18:40:58 +0530

    feat(home): :sparkles: create header layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import Button from '../components/Button/Button'
+import SearchIcon from '../components/Icons/SearchIcon'
+import Settings from '../components/Icons/Settings'
+import Typography from '../components/Typography/Typography'
+
 const Home = () => {
-	return <>Home</>
+	return (
+		<div>
+			<div className='flex justify-between'>
+				<Typography type='title'>Retro Music</Typography>
+				<span className='flex gap-x-3'>
+					<Button className='w-6 h-6'>
+						<SearchIcon fillClassName='fill-dark-100' />
+					</Button>
+					<Button className='w-6 h-6'>
+						<Settings fillClassName='fill-dark-100' />
+					</Button>
+				</span>
+			</div>
+		</div>
+	)
 }
 
 export default Home
