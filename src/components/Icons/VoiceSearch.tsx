cc148f8ccbc8df91e9045a2429465dd6cfd39075
Mayur Panchal <mayur.panchal@deepintent.com>
2022-07-30 09:43:49 +0530

    feat(component): :sparkles: add voice search comp basic

---
+++ src/components/Icons/VoiceSearch.tsx
@@ -0,0 +1,209 @@
+import React from 'react'
+type Props = {
+	className?: string
+}
+
+const VoiceSearch = (props: React.SVGProps<SVGSVGElement> & Props) => {
+	const { className } = props
+	return (
+		<svg
+			className={className}
+			fill='none'
+			height='175'
+			viewBox='0 0 159 175'
+			width='159'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g filter='url(#filter0_dd_124_3510)' opacity='0.3'>
+				<path
+					d='M70 14.3301C74.641 11.6506 80.359 11.6506 85 14.3301L135.385 43.4199C140.026 46.0994 142.885 51.0513 142.885 56.4103V114.59C142.885 119.949 140.026 124.901 135.385 127.58L85 156.67C80.359 159.349 74.641 159.349 70 156.67L19.6151 127.58C14.9741 124.901 12.1151 119.949 12.1151 114.59V56.4103C12.1151 51.0513 14.9741 46.0994 19.6151 43.4199L70 14.3301Z'
+					fill='url(#paint0_linear_124_3510)'
+				/>
+			</g>
+			<g filter='url(#filter1_dd_124_3510)' opacity='0.5'>
+				<path
+					d='M70 33.3301C74.641 30.6506 80.359 30.6506 85 33.3301L118.93 52.9199C123.571 55.5994 126.43 60.5513 126.43 65.9103V105.09C126.43 110.449 123.571 115.401 118.93 118.08L85 137.67C80.359 140.349 74.641 140.349 70 137.67L36.0696 118.08C31.4285 115.401 28.5696 110.449 28.5696 105.09V65.9103C28.5696 60.5513 31.4285 55.5994 36.0696 52.9199L70 33.3301Z'
+					fill='url(#paint1_linear_124_3510)'
+				/>
+			</g>
+			<g filter='url(#filter2_dd_124_3510)'>
+				<path
+					d='M70 49.3301C74.641 46.6506 80.359 46.6506 85 49.3301L105.074 60.9199C109.715 63.5994 112.574 68.5513 112.574 73.9103V97.0897C112.574 102.449 109.715 107.401 105.074 110.08L85 121.67C80.359 124.349 74.641 124.349 70 121.67L49.926 110.08C45.285 107.401 42.426 102.449 42.426 97.0897V73.9103C42.426 68.5513 45.285 63.5994 49.926 60.9199L70 49.3301Z'
+					fill='url(#paint2_linear_124_3510)'
+				/>
+			</g>
+			<g filter='url(#filter3_i_124_3510)'>
+				<path
+					d='M77 90C80.32 90 83 87.32 83 84V72C83 68.68 80.32 66 77 66C73.68 66 71 68.68 71 72V84C71 87.32 73.68 90 77 90ZM88.82 84C87.84 84 87.02 84.72 86.86 85.7C86.04 90.4 81.94 94 77 94C72.06 94 67.96 90.4 67.14 85.7C66.98 84.72 66.16 84 65.18 84C63.96 84 63 85.08 63.18 86.28C64.16 92.28 68.96 96.98 75 97.84V102C75 103.1 75.9 104 77 104C78.1 104 79 103.1 79 102V97.84C85.04 96.98 89.84 92.28 90.82 86.28C91.02 85.08 90.04 84 88.82 84Z'
+					fill='url(#paint3_linear_124_3510)'
+				/>
+			</g>
+			<defs>
+				<filter
+					colorInterpolationFilters='sRGB'
+					filterUnits='userSpaceOnUse'
+					height='174.359'
+					id='filter0_dd_124_3510'
+					width='158.77'
+					x='0.115112'
+					y='0.320496'
+				>
+					<feFlood floodOpacity='0' result='BackgroundImageFix' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='6' dy='6' />
+					<feGaussianBlur stdDeviation='5' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 0.427451 0 0 0 0 0.505882 0 0 0 0 0.631373 0 0 0 0.25 0' />
+					<feBlend in2='BackgroundImageFix' mode='normal' result='effect1_dropShadow_124_3510' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='-6' dy='-6' />
+					<feGaussianBlur stdDeviation='3' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0' />
+					<feBlend in2='effect1_dropShadow_124_3510' mode='normal' result='effect2_dropShadow_124_3510' />
+					<feBlend in='SourceGraphic' in2='effect2_dropShadow_124_3510' mode='normal' result='shape' />
+				</filter>
+				<filter
+					colorInterpolationFilters='sRGB'
+					filterUnits='userSpaceOnUse'
+					height='136.359'
+					id='filter1_dd_124_3510'
+					width='125.861'
+					x='16.5696'
+					y='19.3205'
+				>
+					<feFlood floodOpacity='0' result='BackgroundImageFix' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='6' dy='6' />
+					<feGaussianBlur stdDeviation='5' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 0.427451 0 0 0 0 0.505882 0 0 0 0 0.631373 0 0 0 0.25 0' />
+					<feBlend in2='BackgroundImageFix' mode='normal' result='effect1_dropShadow_124_3510' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='-6' dy='-6' />
+					<feGaussianBlur stdDeviation='3' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0' />
+					<feBlend in2='effect1_dropShadow_124_3510' mode='normal' result='effect2_dropShadow_124_3510' />
+					<feBlend in='SourceGraphic' in2='effect2_dropShadow_124_3510' mode='normal' result='shape' />
+				</filter>
+				<filter
+					colorInterpolationFilters='sRGB'
+					filterUnits='userSpaceOnUse'
+					height='104.359'
+					id='filter2_dd_124_3510'
+					width='98.1479'
+					x='30.426'
+					y='35.3205'
+				>
+					<feFlood floodOpacity='0' result='BackgroundImageFix' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='6' dy='6' />
+					<feGaussianBlur stdDeviation='5' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 0.427451 0 0 0 0 0.505882 0 0 0 0 0.631373 0 0 0 0.25 0' />
+					<feBlend in2='BackgroundImageFix' mode='normal' result='effect1_dropShadow_124_3510' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='-6' dy='-6' />
+					<feGaussianBlur stdDeviation='3' />
+					<feComposite in2='hardAlpha' operator='out' />
+					<feColorMatrix type='matrix' values='0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0' />
+					<feBlend in2='effect1_dropShadow_124_3510' mode='normal' result='effect2_dropShadow_124_3510' />
+					<feBlend in='SourceGraphic' in2='effect2_dropShadow_124_3510' mode='normal' result='shape' />
+				</filter>
+				<filter
+					colorInterpolationFilters='sRGB'
+					filterUnits='userSpaceOnUse'
+					height='52'
+					id='filter3_i_124_3510'
+					width='52'
+					x='49'
+					y='58'
+				>
+					<feFlood floodOpacity='0' result='BackgroundImageFix' />
+					<feBlend in='SourceGraphic' in2='BackgroundImageFix' mode='normal' result='shape' />
+					<feColorMatrix
+						in='SourceAlpha'
+						result='hardAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+					/>
+					<feOffset dx='-4' dy='-4' />
+					<feGaussianBlur stdDeviation='3' />
+					<feComposite in2='hardAlpha' k2='-1' k3='1' operator='arithmetic' />
+					<feColorMatrix type='matrix' values='0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0' />
+					<feBlend in2='shape' mode='normal' result='effect1_innerShadow_124_3510' />
+				</filter>
+				<linearGradient gradientUnits='userSpaceOnUse' id='paint0_linear_124_3510' x1='2' x2='141.675' y1='10' y2='161'>
+					<stop stopColor='#E9F3FB' />
+					<stop offset='1' stopColor='#E4F0FA' />
+				</linearGradient>
+				<linearGradient
+					gradientUnits='userSpaceOnUse'
+					id='paint1_linear_124_3510'
+					x1='21'
+					x2='125.525'
+					y1='29'
+					y2='142'
+				>
+					<stop stopColor='#E9F3FB' />
+					<stop offset='1' stopColor='#E4F0FA' />
+				</linearGradient>
+				<linearGradient
+					gradientUnits='userSpaceOnUse'
+					id='paint2_linear_124_3510'
+					x1='37'
+					x2='111.925'
+					y1='45'
+					y2='126'
+				>
+					<stop stopColor='#E9F3FB' />
+					<stop offset='1' stopColor='#E4F0FA' />
+				</linearGradient>
+				<linearGradient
+					gradientUnits='userSpaceOnUse'
+					id='paint3_linear_124_3510'
+					x1='63.1582'
+					x2='92.3129'
+					y1='84.9997'
+					y2='84.9997'
+				>
+					<stop stopColor='#D58A85' />
+					<stop offset='1' stopColor='#F9B9B0' />
+				</linearGradient>
+			</defs>
+		</svg>
+	)
+}
+
+export default VoiceSearch
